fab9cb19fe7e48d3f362ae5096daf5cd070f0b9b
최봉수 <bongsu@choebongsuui-MacBookAir.local>
2022-01-13 20:48:26 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Opeasdf!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+export default App
 
 const styles = StyleSheet.create({
   container: {
