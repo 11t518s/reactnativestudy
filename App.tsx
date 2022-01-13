2df96d33b2a819d3fa5589272edcf2b252742eac
최봉수 <bongsu@choebongsuui-MacBookAir.local>
2022-01-14 00:46:57 +0900

    ~layout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Opeasdf!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <View style={{flex:1}}>
+          <View style={{flex: 1,backgroundColor: '#111111'}}><Text>1</Text></View>
+          <View style={{flex: 2,backgroundColor: '#222222'}}><Text>2</Text></View>
+          <View style={{flex: 2,backgroundColor: '#333333'}}><Text>3</Text></View>
+          <View style={{flex: 10,backgroundColor: '#444444'}}><Text>4</Text></View>
+          <StatusBar style='auto' />
+        </View>
+      </>
   );
 }
 export default App
@@ -18,4 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    flex: 2,
+    color: '#aaa',
+    fontSize: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
