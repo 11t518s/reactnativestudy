d750c13fbb0c634e50246001a5cabe806afcaf78
최봉수 <bongsu@choebongsuui-MacBookAir.local>
2022-01-14 10:51:16 +0900

    location

--- App.tsx
+++ App.tsx
@@ -1,33 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 const App = () => {
+  const [city, setCity] = useState("loading~~~");
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      {
+        useGoogleMaps: false,
+      }
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
-      <>
-        <View style={{flex:1}}>
-          <View style={{flex: 1,backgroundColor: '#111111'}}><Text>1</Text></View>
-          <View style={{flex: 2,backgroundColor: '#222222'}}><Text>2</Text></View>
-          <View style={{flex: 2,backgroundColor: '#333333'}}><Text>3</Text></View>
-          <View style={{flex: 10,backgroundColor: '#444444'}}><Text>4</Text></View>
-          <StatusBar style='auto' />
+    <>
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>{city}</Text>
         </View>
-      </>
+        <ScrollView
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.weather}
+        >
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>sunny</Text>
+          </View>
+        </ScrollView>
+        <StatusBar style="light" />
+      </View>
+    </>
   );
-}
-export default App
+};
+export default App;
+
+const { height, width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
   },
-  text: {
-    flex: 2,
-    color: '#aaa',
-    fontSize: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+  city: {
+    flex: 1.2,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  day: {
+    width: width,
+    alignItems: "center",
+  },
+  weather: {
+    backgroundColor: "blue",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 168,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
 });
