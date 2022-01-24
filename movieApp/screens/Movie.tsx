5be97d4f13d3480bceac90f98bc7a31f01f1389b
최봉수 <11t518s20161759@gmail.com>
2022-01-24 13:19:11 +0900

    ~ movie screen

--- movieApp/screens/Movie.tsx
+++ movieApp/screens/Movie.tsx
@@ -1,26 +1,127 @@
-import React from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useState, useEffect } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+  Appearance,
+} from "react-native";
 import styled from "styled-components/native";
+import Swiper from "react-native-swiper";
+import { makeImgPath } from "../utills";
+import { BlurView } from "expo-blur";
 
-const Btn = styled.Pressable`
+const API_KEY = "c5aacf90825fb4feae3debb9eff0ae56";
+
+const Container = styled.ScrollView``;
+
+const Loader = styled.View`
   flex: 1;
   justify-content: center;
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const BgImg = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-around;
   align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
+`;
+const Column = styled.View`
+  width: 60%;
+`;
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+const Votes = styled(Overview)`
+  font-size: 12px;
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.mainBgColor};
-`;
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'> = ({
-  navigation,
-}) => (
-  <Btn
-    onPress={() => navigation.navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alienItems: "center" }}
-  >
-    <Title>Movie</Title>
-  </Btn>
-);
-
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [nowPlayingMovies, setNowPlayingMovies, useColorScheme] = useState([]);
+  //   const isDark = useColorScheme() === "dark" ;
+  const isDark = false;
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setNowPlayingMovies(results);
+    console.log(results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="small" />
+    </Loader>
+  ) : (
+    <Container>
+      <Swiper
+        loop
+        horizontal
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        {nowPlayingMovies.map((movie) => (
+          <View key={movie.id}>
+            <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} />
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={85}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title isDark={isDark}>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes isDark={isDark}>⭐️ {movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview isDark={isDark}>
+                    {movie.overview.slice(0, 100)}...
+                  </Overview>
+                </Column>
+              </Wrapper>
+            </BlurView>
+          </View>
+        ))}
+      </Swiper>
+    </Container>
+  );
+};
 export default Movies;
