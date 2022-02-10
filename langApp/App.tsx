66bda1156f252122590c4e54af1abc49548f715e
최봉수 <11t518s20161759@gmail.com>
2022-02-11 01:46:46 +0900

    ~Animate and togle and useRef

--- langApp/App.tsx
+++ langApp/App.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
-import styled from 'styled-componenets/native'
+import React, { useRef, useState } from "react";
+import { Animated, Easing, Pressable } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  return null
+  const [up, setUp] = useState(false);
+  const Y = useRef(new Animated.Value(200)).current;
+  const toggleUp = () => setUp(!up);
+  const moveUp = () => {
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
+      useNativeDriver: true,
+    }).start(toggleUp);
+  };
+
+  return (
+    <Container>
+      <Pressable onPress={moveUp}>
+        <AnimatedBox
+          style={{
+            transform: [{ translateY: Y }],
+          }}
+        />
+      </Pressable>
+    </Container>
+  );
 }
