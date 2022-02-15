995eeb36352ed3523a71272ae5ae379a3fb63056
최봉수 <11t518s20161759@gmail.com>
2022-02-15 13:22:57 +0900

    card animation

--- langApp/App.tsx
+++ langApp/App.tsx
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Animated, PanResponder, Text, View } from "react-native";
 import styled from "styled-components/native";
+import numbers from "./numbers";
 
 const Container = styled.View`
   flex: 1;
@@ -17,39 +18,117 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
 `;
 function App() {
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    extrapolate: "clamp",
+  });
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
+  });
+  const onPressIn = () =>
+    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true });
+  const onPressOut = Animated.timing(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 10,
+    restSpeedThreshold: 10,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 100,
+    restSpeedThreshold: 100,
+  });
+
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (_, { dx }) => {
         position.setValue(dx);
       },
-      onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
-          Animated.spring(position, {
-            toValue: 0,
-            useNativeDriver: true,
-          }),
-        ]).start();
+      onPanResponderGrant: () => onPressIn().start(),
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -200) {
+          goLeft.start(onDismiss);
+        } else if (dx > 200) {
+          goRight.start(onDismiss);
+        } else Animated.parallel([onPressOut, goCenter]).start();
       },
     })
   ).current;
-  const scale = useRef(new Animated.Value(1)).current;
-  const position = useRef(new Animated.Value(0)).current;
-  const onPressIn = () =>
-    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
-  const onPressOut = Animated.spring(scale, {
-    toValue: 1,
-    useNativeDriver: true,
-  });
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex(index + 1);
+  };
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
+
   return (
     <Container>
-      <Card style={{ transform: [{ scale }] }}>
-        <Text>test</Text>
-      </Card>
+      <CardContainer>
+        <Card style={{ transform: [{ scale: secondScale }] }}>
+          <Text>{numbers[index + 1]}</Text>
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              { scale },
+              { translateX: position },
+              { rotate: rotation },
+            ],
+          }}
+        >
+          <Text>{numbers[index]}</Text>
+        </Card>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={closePress}>
+          <Text>close</Text>
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Text>check</Text>
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 }
